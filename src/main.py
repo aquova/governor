21c8a7221cbdde146b0bfc6e1d74062cb5502be1
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2022-07-16 03:31:27 +0100

    If a log is sent as discord attachment, it will upload it to the SMAPI site. (#24)
    * will now upload the log to smapi site
    
    * updated per aquo's request

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import string
 
 import discord
 import requests
+import urllib.parse
 
 import commonbot.utils
 from commonbot.debug import Debug
@@ -170,6 +171,18 @@ async def on_message(message: discord.Message):
             if windows_info:  # Condense OS text for Windows because it's often quite verbose.
                 log_dict["OS"] = windows_info.group(1)
             await message.channel.send(smapi_log_message_template.substitute(log_dict))
+
+    for attachment in message.attachments:
+        if attachment.filename == "SMAPI-latest.txt" or attachment.filename == "SMAPI-crash.txt":
+            r = requests.get(attachment.url)
+            log = urllib.parse.quote(r.text)
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
+
+            s = requests.post('https://smapi.io/log/', data="input={0}".format(log), headers=headers)
+            logurl = s.text.split('</strong> <code>')[1].split('</code>')[0]
+            await message.channel.send("Log found, uploaded to: " + logurl)
 
 
     # Check if someone is trying to use a bot command
