889cc386ee94c4507096e44b6093dc5fdd1ab14b
aquova <mail@aquova.net>
2024-04-09 21:58:40 -0500

    Add expand/compress buttons to commands page

--- html/commands.php
+++ html/commands.php
@@ -6,12 +6,27 @@
         <link rel="icon" href="assets/cojiro.png" type="image/png">
         <link rel="stylesheet" href="css/base.css" type="text/css">
         <link rel="stylesheet" href="css/commands.css" type="text/css">
+        <script>
+            function expandAll(expand) {
+                for (const element of document.querySelectorAll("details"))
+                    if (expand) {
+                        element.setAttribute("open", "")
+                    } else {
+                        element.removeAttribute("open")
+                    }
+                }
+        </script>
     </head>
     <body>
         <header>
             <h1>Discord Custom Commands</h1>
         </header>
         <main>
+            <div class="center">
+                <button onclick="expandAll(true)">Expand All</button>
+                <button onclick="expandAll(false)">Compress All</button>
+            </div>
+            <br/>
             <?php
                 include 'db.php';
                 populate_cmd_tbl();
