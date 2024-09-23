4333888eda56750209fca628b4d9d186a8923b39
aquova <mail@aquova.net>
2024-09-23 13:57:43 -0500

    Improve accessibility of webpages

--- html/commands.php
+++ html/commands.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>Custom Commands</title>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="assets/cojiro.png" type="image/png">
         <link rel="stylesheet" href="css/base.css" type="text/css">
         <link rel="stylesheet" href="css/commands.css" type="text/css">
