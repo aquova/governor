7fea16729fe45483266ef2500e0d807e4fcd4191
aquova <mail@aquova.net>
2024-09-24 10:39:51 -0500

    Check for roles more readily

--- src/tracker.py
+++ src/tracker.py
@@ -10,8 +10,6 @@ from config import RANKS, XP_PER_LVL, XP_PER_MINUTE
 class Tracker(commands.Cog):
     def __init__(self):
         self.user_cache: dict[int, db.UserData] = {}
-        # Maps uids to their index in RANKS
-        self.role_cache: dict[int, int] = {}
         self.xp_multiplier = 1
 
     def setup(self, guild: discord.Guild):
@@ -52,20 +50,7 @@ class Tracker(commands.Cog):
 
         if user.id not in self.user_cache:
             self.user_cache[user.id] = db.fetch_user_data(user.id)
-
-        if user.id not in self.role_cache:
-            # This is the highest role *they have*, not the highest they should
-            # We'll check and remedy that in a moment, if needed
-            role_ids = [x.id for x in user.roles]
-            best_idx = 0
-            for idx in range(0, len(RANKS))[::-1]:
-                if RANKS[idx]['role_id'] in role_ids:
-                    best_idx = idx
-                    break
-            self.role_cache[user.id] = best_idx
-
         user_data = self.user_cache[user.id]
-        role_idx = self.role_cache[user.id]
 
         # Check their last timestamp if we aren't manually adding XP
         if not manual:
@@ -83,49 +68,44 @@ class Tracker(commands.Cog):
             # We don't consider manually added XP as part of the monthly total
             user_data.monthly_xp += xp_add
 
-        ret = ""
-        # If they haven't maxed out their roles, and have enough XP for the next one, grant that Discord role and post messages
-        if role_idx < len(RANKS) - 1:
-            next_xp = RANKS[role_idx + 1]['level'] * XP_PER_LVL
-            if next_xp < user_data.xp:
-                role_idx += 1
-                self.role_cache[user.id] = role_idx
-                roles = user.roles
-                new_role = discord.utils.get(user.guild.roles, id=RANKS[role_idx]['role_id'])
-                if new_role is not None:
-                    roles.append(new_role)
-                    await user.edit(roles=roles)
-                # Some ranks have special messages, either for the user or for a new channel they've just unlocked
-                if RANKS[role_idx]['message'] != "":
-                    ret = f"<@{user.id}> {RANKS[role_idx]['message']}"
-                if RANKS[role_idx]['welcome']['message'] != "":
-                    for welcome_id in RANKS[role_idx]['welcome']['channels']:
-                        chan = cast(discord.TextChannel, self.server.get_channel(welcome_id))
-                        if chan is not None:
-                            await chan.send(f"Hello <@{user.id}>! {RANKS[role_idx]['welcome']['message']}")
-
+        ret = await self._check_missing_roles(user, user_data, True)
         self.user_cache[user.id] = user_data
-        self.role_cache[user.id] = role_idx
         db.set_user_data(user, user_data)
         return ret
 
     async def bring_up_user(self, user: discord.Member):
         # When a user joins the server, give them any roles they need
         user_data = db.fetch_user_data(user.id)
+        await self._check_missing_roles(user, user_data, False)
+
+    async def _check_missing_roles(self, user: discord.Member, data: db.UserData, post_messages: bool) -> str:
         roles = user.roles
         role_ids = [x.id for x in user.roles]
-        for idx in range(0, len(RANKS)):
-            if RANKS[idx]['level'] * XP_PER_LVL < user_data.xp and RANKS[idx]['role_id'] not in role_ids:
-                new_role = discord.utils.get(user.guild.roles, id=RANKS[idx]['role_id'])
-                if new_role is not None:
-                    roles.append(new_role)
+        ret = ""
+        for rank in RANKS:
+            if data.xp < rank['level'] * XP_PER_LVL or rank['role_id'] in role_ids:
+                continue
+
+            new_role = discord.utils.get(user.guild.roles, id=rank['role_id'])
+            if new_role is not None:
+                roles.append(new_role)
+            if not post_messages:
+                continue
+            # Some ranks have special messages, either for the user or for a new channel they've just unlocked
+            if rank['message'] != "":
+                # We'll overwrite the messages for the lower ranks, so we don't spam the user
+                ret = f"<@{user.id}> {rank['message']}"
+            if rank['welcome']['message'] != "":
+                for welcome_id in rank['welcome']['channels']:
+                    chan = cast(discord.TextChannel, self.server.get_channel(welcome_id))
+                    if chan is not None:
+                        await chan.send(f"Hello <@{user.id}>! {rank['welcome']['message']}")
         await user.edit(roles=roles)
+        return ret
 
     def remove_from_cache(self, uid: int):
         if uid in self.user_cache:
             del self.user_cache[uid]
-        if uid in self.role_cache:
-            del self.role_cache[uid]
 
     async def set_bonus_xp(self, enabled: bool) -> str:
         if enabled:
