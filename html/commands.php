7de844dcf7918ca5817bc611ce250db1170b80e4
aquova <mail@aquova.net>
2024-09-25 17:44:36 -0500

    Remove problematic meta tag from webpages

--- html/commands.php
+++ html/commands.php
@@ -3,7 +3,6 @@
     <head>
         <title>Custom Commands</title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="assets/cojiro.png" type="image/png">
         <link rel="stylesheet" href="css/base.css" type="text/css">
         <link rel="stylesheet" href="css/commands.css" type="text/css">
--- html/leaderboard.php
+++ html/leaderboard.php
@@ -3,7 +3,6 @@
     <head>
         <title>Leaderboard</title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="assets/cojiro.png" type="image/png">
         <link rel="stylesheet" href="css/base.css" type="text/css">
         <link rel="stylesheet" href="css/leaderboard.css" type="text/css">
